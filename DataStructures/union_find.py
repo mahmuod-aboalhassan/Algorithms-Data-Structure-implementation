e9ef18ee91cef83bb3770f505fa1cfb85e9d38e8
mahmuodsmsm <mahmuodaboalhassan@gmail.com>
2024-09-15 14:01:30 +0300

    adding rank approach

--- DataStructures/union_find.py
+++ DataStructures/union_find.py
@@ -1,28 +1,46 @@
 class Union_Find:
-    def __init__(self,N: int) -> None:
-        self.parent=list(range(N))
-        self.rank=[1]*N
-    def find(self,i):
-        while i!= self.parent[i]:i=self.parent[i]
-        return i 
-    def connected(self,p,q):
-        return self.parent[p]== self.parent[q]
-    def union(self,p,q):
-        i=self.find(p)
-        j=self.find(q)
-        self.parent[i]=j
+    def __init__(self, N: int) -> None:
+        self.parent = list(range(N))
+        self.rank = [1] * N  # Rank represents the height of the tree.
 
+    def find(self, i):
+        # Path compression heuristic
+        if i != self.parent[i]:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def connected(self, p, q):
+        return self.find(p) == self.find(q)
+
+    def union(self, p, q):
+        i = self.find(p)
+        j = self.find(q)
+        
+        if i == j:
+            return  # Already in the same set
+        
+        # Union by rank: Attach smaller tree under the root of the larger tree.
+        if self.rank[i] > self.rank[j]:
+            self.parent[j] = i
+        elif self.rank[i] < self.rank[j]:
+            self.parent[i] = j
+        else:
+            self.parent[j] = i
+            self.rank[i] += 1  # Increase rank when ranks are equal
+
+# Testing the corrected implementation
 uf = Union_Find(10)
 
 # Perform union operations to connect elements
-uf.union(1, 2)  # Connect component containing 1 with component containing 2
-uf.union(3, 4)  # Connect component containing 3 with component containing 4
-uf.union(1, 4)  # Connect component containing 1 (and thus 2) with component containing 4 (and thus 3)
-uf.union(5, 6)  # Connect component containing 5 with component containing 6
-uf.union(5, 9)  # Connect component containing 5 (and thus 6) with component containing 9
-uf.union(0, 5)  # Connect component containing 0 with component containing 5 (and thus 6, 9)
-uf.union(7, 8)  # Connect component containing 7 with component containing 8
-uf.union(7, 0)  # Connect component containing 7 (and thus 8) with component containing 0 (and thus 5, 6, 9)
+uf.union(1, 2)
+uf.union(3, 4)
+uf.union(1, 4)
+uf.union(5, 6)
+uf.union(5, 9)
+uf.union(0, 5)
+uf.union(7, 8)
+uf.union(7, 0)
 
 # Output the current state of the array, which represents the connected components
-print(uf.parent)  # Expected output: [9, 2, 4, 4, 4, 6, 9, 8, 9, 9]
+print(uf.parent)  # This will show the parent of each element
+print(uf.rank)    # This shows the rank (tree height) of each component's root
